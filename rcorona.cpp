f1d56ed96154e133b6c809df50e5a1ea01875d7c
blackcorona43 <60207075+blackcorona43@users.noreply.github.com>
2022-10-04 09:50:28 -0700

    Add files via upload

--- rcorona.cpp
+++ rcorona.cpp
@@ -2,11 +2,22 @@
 //2 player snake code
 //
 #include <stdio.h>
+#define MAX_GRID 80
 
-int name3()
+
+/*typedef struct a_snake
 {
-    printf("Ruben \n");
-    return 0;
+    int status;
+    int length;
+    int pos[MAX_GRID*MAX_GRID][2];
+    int direction;
+    double timer;
+    double delay;
+}Ai_snake;*/
+
+
+unsigned int computer_snake(unsigned int state)
+{
+    state = state ^ 1;
+    return state;
 }
-
-
---
+++ rcorona.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#define MAX_GRID 80
+
+
+typedef struct a_snake
+{
+    int status;
+    int length;
+    int pos[MAX_GRID*MAX_GRID][2];
+    int direction;
+    double timer;
+    double delay;
+}Ai_snake;
+
+
+extern unsigned int computer_snake(unsigned int state);
+
+
+
