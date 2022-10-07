862dcd182a6877453e50498e08050d2c61c04849
singhjasdeep13 <98551356+singhjasdeep13@users.noreply.github.com>
2022-10-07 09:52:33 -0700

    Add files via upload

--- jsingh.h
+++ jsingh.h
@@ -1,4 +1,15 @@
 // Jasdeep Singh - header file
 // include header guard
 
+typedef struct t_button {
+    Rect r;
+    char text[32];
+    int over;
+    int down;
+    int click;
+    float color[3];
+    float dcolor[3];
+    unsigned text color;
+} IntroButtons;
+
 extern void showIntro(int, int);
