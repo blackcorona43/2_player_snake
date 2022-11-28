fde8a5601f213c791b94567d4ed164e252d6a92e
stellez4 <111898074+stellez4@users.noreply.github.com>
2022-11-28 09:01:35 -0800

    Add files via upload

--- stellez.cpp
+++ stellez.cpp
@@ -1,180 +1,290 @@
 //Samantha Tellez
 //my new source file
 //no main!
+
 #include <stdio.h>
 #include <cstring>
 #include <cstdlib>
 #include <unistd.h>
-/*#include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <math.h>
 #include <X11/Xlib.h>
-#include <X11/keysym.h>*/
+#include <X11/keysym.h>
+#include <time.h>
 #include <GL/glx.h>
+//#include <math.h>
+//#include <random>
+//#include <math.h>
+//#include <cmath>
+//#include <ctime>
 #include "log.h"
 #include "fonts.h"
 
 int show_my_name()
 {
-	printf("samantha\n");
-	return 0;
+    printf("samantha\n");
+    return 0;
 }
 
-class MyImage {
-	public:
-		int width, height;
-		unsigned char *data;
-		~MyImage(); //destructor
-		MyImage(const char *fname);
-};
+//=============================
+//     For credits image
+//=============================
 
-//extern class MyImage myimage;
+class MyImage {
+    public:
+        int width, height;
+        unsigned char *data;
+        ~MyImage(); //destructor
+        MyImage(const char *fname);
+};
 
 MyImage::~MyImage() { delete [] data; }
 
 MyImage::MyImage(const char *fname) {
-	//if (fname[0] == '\0')
-	if (strlen(fname) == 0)
-		return;
-	//apply constants later
-	//char name[FILENAME_MAX];
-	char name[40];
-	strcpy(name, fname);
-	int slen = strlen(name);
-	name[slen-4] = '\0';
-	char ppmname[80];
-	sprintf(ppmname,"%s.ppm", name);
-	char ts[100];
-	sprintf(ts, "convert %s %s", fname, ppmname);
-	system(ts);
-	FILE *fpi = fopen(ppmname, "r");
-	if (fpi) {
-		char line[200];
-		fgets(line, 200, fpi);
-		fgets(line, 200, fpi);
-		//skip comments and blank lines
-		while (line[0] == '#' || strlen(line) < 2)
-			fgets(line, 200, fpi);
-		sscanf(line, "%i %i", &width, &height);
-		fgets(line, 200, fpi);
-		//get pixel data
-		int n = width * height * 3;
-		data = new unsigned char[n];
-		for (int i=0; i<n; i++)
-			data[i] = fgetc(fpi);
-		fclose(fpi);
-	} else {
-		printf("ERROR opening image: %s\n", ppmname);
-		exit(0);
-	}
-	unlink(ppmname);
+    if (strlen(fname) == 0)
+        return;
+    char name[40];
+    strcpy(name, fname);
+    int slen = strlen(name);
+    name[slen-4] = '\0';
+    char ppmname[80];
+    sprintf(ppmname,"%s.ppm", name);
+    char ts[100];
+    sprintf(ts, "convert %s %s", fname, ppmname);
+    system(ts);
+    FILE *fpi = fopen(ppmname, "r");
+    if (fpi) {
+        char line[200];
+        fgets(line, 200, fpi);
+        fgets(line, 200, fpi);
+        //skip comments and blank lines
+        while (line[0] == '#' || strlen(line) < 2)
+            fgets(line, 200, fpi);
+        sscanf(line, "%i %i", &width, &height);
+        fgets(line, 200, fpi);
+        //get pixel data
+        int n = width * height * 3;
+        data = new unsigned char[n];
+        for (int i=0; i<n; i++)
+            data[i] = fgetc(fpi);
+        fclose(fpi);
+    } else {
+        printf("ERROR opening image: %s\n", ppmname);
+        exit(0);
+    }
+    unlink(ppmname);
 }
+
 class MyImage myimage = {"images/allsnake.jpg"};
 MyImage imgg[1] = {"images/allsnake.jpg"};
 
 class Texture {
-	public:
-		//change Image to MyImage class
-		//Image *backImage;
-		MyImage *backImage;
-		GLuint backTexture;
-		float xc[2];
-		float yc[2];
+    public:
+        MyImage *backImage;
+        GLuint backTexture;
+        float xc[2];
+        float yc[2];
 };
 
 class Globe {
-	public:
-		int xres, yres;
-		Texture tex;
-		//Texture tex2;
-		Globe() {
-			xres=640, yres=480;
-			//xres=950, yres=690;
-		}
+    public:
+        int xres, yres;
+        int movement;
+        int movement2;
+        Texture tex;
+        Globe() {
+            xres = 2400;
+            yres = 1800;
+            movement = 1;
+            movement2 = 1;
+        }
 } gg;
+
+//=================================
+//     Stuff for Feature Mode
+//=================================
+
+//Notes:
+//particles/movement = right
+//particles2/movement2 = left
+
+const int MAX_PARTICLES = 10000;
+
+class Box {
+    public:
+        float w;
+        float dir;
+        float vel[2];
+        float pos[2];
+        Box() {
+            w = 0.0f; //makes box go straight down
+            dir = 25.0f;
+            pos[0] = gg.xres / 2.0f;
+            pos[1] = gg.yres / 2.0f;
+            vel[0] = vel[1] = 0.0;
+        }
+        Box(float wid, float d, float p0, float p1) {
+            w = wid;
+            dir = d;
+            pos[0] = p0;
+            pos[1] = p1;
+            vel[0] = vel[1] = 0.0;
+        }
+} box, particles[MAX_PARTICLES], particles2[MAX_PARTICLES]; 
+
+int n = 0;
+
+void make_particles(int x, int y)
+{
+    if (n >= MAX_PARTICLES)
+        return;
+    particles[n].w = 25.0; //size of particles
+    particles[n].pos[0] = x;
+    particles[n].pos[1] = y;
+    particles[n].vel[0] = particles[n].vel[1] = 0.0;
+    ++n;
+
+    particles2[n].w = 25.0; //size of particles
+    particles2[n].pos[0] = x;
+    particles2[n].pos[1] = y;
+    particles2[n].vel[0] = particles2[n].vel[1] = 0.0;
+    ++n;
+}
+
+void physicsFeat()
+{
+    //particles1
+    if (gg.movement < 500)
+        gg.movement += 40; //how far apart the squares are
+    if (gg.movement >= 500)
+        gg.movement -= 500; //how wide the group of squares are
+
+    make_particles(box.pos[0]+gg.movement, box.pos[1]*2);
+
+    //particles2
+    if (gg.movement2 < -10)
+        gg.movement2 += 40; //how far apart the squares are
+    if (gg.movement2 >= -10)
+        gg.movement2 -= 500; //how wide the group of squares are
+
+    make_particles(box.pos[0]+gg.movement2, box.pos[1]*2);
+
+    for (int i=0; i<n; i++)
+    {
+        particles[i].vel[1] -= 0.10; //speed of squares
+        particles[i].pos[0] += particles[i].vel[0];
+        particles[i].pos[1] += particles[i].vel[1];
+
+        if (particles[i].pos[1] < 0.0)
+            particles[i] = particles[--n];
+    }
+
+    //particles2
+    /*if (gg.movement2 < -10)
+      gg.movement2 += 40; //how far apart the squares are
+      if (gg.movement2 >= -10)
+      gg.movement2 -= 500; //how wide the group of squares are
+    //gg.movement -= 1000;
+
+    make_particles(box.pos[0]+gg.movement2, box.pos[1]*2);
+
+    for (int i=0; i<n; i++)
+    {
+    particles2[i].vel[1] -= 0.10; //speed of squares
+    particles2[i].pos[0] += particles2[i].vel[0];
+    particles2[i].pos[1] += particles2[i].vel[1];
+
+    if (particles2[i].pos[1] < 0.0)
+    particles2[i] = particles2[--n];
+    }*/
+
+}
+
+void makeFeat()
+{
+    //Draw all particle
+    for (int i=0; i<n; i++)
+    {
+        glPushMatrix();
+        glColor3ub(150, 160, 255);
+        glTranslatef(particles[i].pos[0], particles[i].pos[1], 0.0f);
+        glBegin(GL_QUADS);
+        glVertex2f(-particles[i].w, -particles[i].w);
+        glVertex2f(-particles[i].w,  particles[i].w);
+        glVertex2f( particles[i].w,  particles[i].w);
+        glVertex2f( particles[i].w, -particles[i].w);
+        glEnd();
+        glTranslatef(particles2[i].pos[0], particles2[i].pos[1], 0.0f);
+        glBegin(GL_QUADS);
+        glVertex2f(-particles2[i].w, -particles2[i].w);
+        glVertex2f(-particles2[i].w,  particles2[i].w);
+        glVertex2f( particles2[i].w,  particles2[i].w);
+        glVertex2f( particles2[i].w, -particles2[i].w);
+        glEnd();
+        glPopMatrix();
+    }
+
+    //Draw all particle2
+    /*for (int i=0; i<n; i++)
+      {
+      glPushMatrix();
+      glColor3ub(150, 160, 255);
+      glTranslatef(particles2[i].pos[0], particles2[i].pos[1], 0.0f);
+      glBegin(GL_QUADS);
+      glVertex2f(-particles2[i].w, -particles2[i].w);
+      glVertex2f(-particles2[i].w,  particles2[i].w);
+      glVertex2f( particles2[i].w,  particles2[i].w);
+      glVertex2f( particles2[i].w, -particles2[i].w);
+      glEnd();
+      glPopMatrix();
+      }*/
+}
 
 unsigned int manage_state_st(unsigned int s)
 {
-	//manages credit to turn off/on
-	s = s ^ 1;
-	return s;
+    //manages credit to turn off/on
+    s = s ^ 1;
+    return s;
 }
 
 void show_credits(int xres, int yres)
 {
-	/*Rect r;
-	int w = 10;
-	//static float w = 400.0f;
-	static float pos[2] = {0.0f+w, yres/2.0f};
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPushMatrix();
-	glColor3ub(217,70,40);
-	//glColor3ub(150,160,220);
-	glTranslatef(pos[0], pos[1], 0.0f);
-	glBegin(GL_QUADS);
-	glVertex2f(xres,-yres);
-	glVertex2f(xres, yres);
-	glVertex2f(xres, yres);
-	glVertex2f(xres,-yres);
-	glEnd();
-	glPopMatrix();
+    //credits pic
+    int wid;
+    int hei;
 
-	r.left   = xres/2;
-	//r.left   = xres/2;
-	//r.bot    = yres-100;
-	//r.bot    = yres-300; //centered
-	r.bot    = yres-150;
-	r.center = 1;
-	ggprint16(&r, 16, 0x00ffffff, "--Credits--");
-	ggprint16(&r, 16, 0x00ffffff, " ");
-	ggprint16(&r, 16, 0x00ffffff, "Project Made By:");
-	ggprint16(&r, 16, 0x00ffffff, " ");
-	ggprint16(&r, 16, 0x00ffffff, "Gabriel Aponte, Dominic Flores");
-	ggprint16(&r, 16, 0x00ffffff, " ");
-	ggprint16(&r, 16, 0x00ffffff, "Ruben Corona, Samantha Tellez, and Jasdeep Singh");*/
+    //init_opengl
+    gg.tex.backImage = &imgg[0];
+    //create opengl texture elements
+    glGenTextures(1, &gg.tex.backTexture);
+    wid = gg.tex.backImage->width;
+    hei = gg.tex.backImage->height;
+    glBindTexture(GL_TEXTURE_2D, gg.tex.backTexture);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, wid, hei, 0,
+            GL_RGB, GL_UNSIGNED_BYTE, gg.tex.backImage->data);
+    gg.tex.xc[0] = 0.0;
+    gg.tex.xc[1] = 1.0;
+    gg.tex.yc[0] = 0.0;
+    gg.tex.yc[1] = 1.0;
 
-	//
-	//credits pic
-	int wid;
-	int hei;
-
-	//init_opengl
-	//
-	//this is the main background texture map
-	gg.tex.backImage = &imgg[0];
-	//create opengl texture elements
-	glGenTextures(1, &gg.tex.backTexture);
-	wid = gg.tex.backImage->width;
-	hei = gg.tex.backImage->height;
-	glBindTexture(GL_TEXTURE_2D, gg.tex.backTexture);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, 3, wid, hei, 0,
-			GL_RGB, GL_UNSIGNED_BYTE, gg.tex.backImage->data);
-	gg.tex.xc[0] = 0.0;
-	//gg.tex.xc[1] = 0.25;
-	gg.tex.xc[1] = 1.0;
-	gg.tex.yc[0] = 0.0;
-	gg.tex.yc[1] = 1.0;
-
-	//render section:
-	//this is our main background picture
-	glColor3f(1.0, 1.0, 1.0);
-	glBindTexture(GL_TEXTURE_2D, gg.tex.backTexture);
-	glBegin(GL_QUADS);
-	glTexCoord2f(gg.tex.xc[0], gg.tex.yc[1]); glVertex2i(0, 0);
-	glTexCoord2f(gg.tex.xc[0], gg.tex.yc[0]); glVertex2i(0, yres);
-	glTexCoord2f(gg.tex.xc[1], gg.tex.yc[0]); glVertex2i(xres, yres);
-	glTexCoord2f(gg.tex.xc[1], gg.tex.yc[1]); glVertex2i(xres, 0);
-	//glTexCoord2f(gg.tex.xc[0], gg.tex.yc[0]); glVertex2i(0,      gg.yres);
-	//glTexCoord2f(gg.tex.xc[1], gg.tex.yc[0]); glVertex2i(gg.xres, gg.yres);
-	//glTexCoord2f(gg.tex.xc[1], gg.tex.yc[1]); glVertex2i(gg.xres, 0);
-	glEnd();
+    //render section:
+    glColor3f(1.0, 1.0, 1.0);
+    glBindTexture(GL_TEXTURE_2D, gg.tex.backTexture);
+    glBegin(GL_QUADS);
+    glTexCoord2f(gg.tex.xc[0], gg.tex.yc[1]); glVertex2i(0, 0);
+    glTexCoord2f(gg.tex.xc[0], gg.tex.yc[0]); glVertex2i(0, yres);
+    glTexCoord2f(gg.tex.xc[1], gg.tex.yc[0]); glVertex2i(xres, yres);
+    glTexCoord2f(gg.tex.xc[1], gg.tex.yc[1]); glVertex2i(xres, 0);
+    glEnd();
 
 }
 
-//Feature Mode
+//=================================
+//      Feature Mode Function
+//=================================
+
 void show_feature_st(int xres, int yres)
 {
     //text for feature mode
@@ -182,15 +292,20 @@ void show_feature_st(int xres, int yres)
     r.left   = xres/2;
     r.bot    = yres-150;
     r.center = 1;
-    ggprint16(&r, 16, 0x00df9fdf, "SAMANTHA'S FEATURE MODE");
+    ggprint16(&r, 16, 0x004da6ff, "SAMANTHA'S FEATURE MODE");
+
+    //side text
+    Rect s;
+    s.left   = xres/7;
+    s.bot    = yres-700;
+    s.center = 1;
+    ggprint16(&s, 16, 0x0099d6ff, "The rain doesn't fall perfectly because wind is blowing it (;");
+
 
     //border
-    //
-    //
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //transparent
     glEnable(GL_BLEND);
-    //glColor3f(1.0, 1.0, 0.0);
-    glColor4f(0.6, 0.5, 1.5, 0.6); //red, green, blue, alpha
+    glColor4f(0.0, 0.0, 1.5, 0.6); //red, green, blue, alpha
     int w = 25;
     glBegin(GL_TRIANGLE_STRIP);
     glVertex2i(0, 0);
@@ -211,74 +326,26 @@ void show_feature_st(int xres, int yres)
     glEnd();
     glDisable(GL_BLEND);
 
-    //feature part
+    //feature mode part
     //
     //
-    //Box 1 (middle)
-    //          From struct Global
-    int boardDim;
-    int gridDim=15; //size of square
-    //          From init()
-    boardDim = gridDim*32;
-    //          From render
-    int s0 = xres>>1;
-    int s1 = yres>>1;
-    int b2 = boardDim/2;
-    //          From asteroids game
+    //
+
+    //background color
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //transparent
     glEnable(GL_BLEND);
-    glColor4f(1.6, 0.5, 1.5, 1.0); //red, green, blue, alpha
+    glColor4f(0.1, 0.1, 0.1, 0.3); //red, green, blue, alpha
     glBegin(GL_QUADS);
-    //          From render
-    glVertex2i(s0-b2, s1-b2);
-    glVertex2i(s0-b2, s1+b2);
-    glVertex2i(s0+b2, s1+b2);
-    glVertex2i(s0+b2, s1-b2);
+    glVertex2i(-xres, -yres);
+    glVertex2i(-xres, yres);
+    glVertex2i(xres, yres);
+    glVertex2i(xres, -yres);
     glEnd();
-    //
-    //Box 2 (lower left)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_BLEND);
-    glColor4f(2.6, 0.5, 0.5, 1.0);
-    int s0_2 = xres>>2;
-    int s1_2 = yres>>2;
-    glBegin(GL_QUADS);
-    glVertex2i(s0_2-b2, s1_2-b2);
-    glVertex2i(s0_2-b2, s1_2+b2);
-    glVertex2i(s0_2+b2, s1_2+b2);
-    glVertex2i(s0_2+b2, s1_2-b2);
-    glEnd();
-    //
-    //Box 3 (mid bottom)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_BLEND);
-    glColor4f(1.0, 2.5, 0.5, 1.0);
-    int s0_3 = xres>>1;
-    int s1_3 = yres>>3;
-    glBegin(GL_QUADS);
-    glVertex2i(s0_3-b2, s1_3-b2);
-    glVertex2i(s0_3-b2, s1_3+b2);
-    glVertex2i(s0_3+b2, s1_3+b2);
-    glVertex2i(s0_3+b2, s1_3-b2);
-    glEnd();
-    //
-    //Box 4 (blue square)
-    /*int boardDim_2;
-    int gridDim_2=25;
-    boardDim_2 = gridDim_2*32;
-    int s0_4 = xres>>2;
-    int s1_4 = yres>>1;
-    int b2_2 = boardDim_2/2;
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_BLEND);
-    glColor4f(0.5, 0.5, 3.5, 1.0);
-    glBegin(GL_QUADS);
-    glVertex2i(s0_4-b2_2, s1_4-b2_2);
-    glVertex2i(s0_4-b2_2, s1_4+b2_2);
-    glVertex2i(s0_4+b2_2, s1_4+b2_2);
-    glVertex2i(s0_4+b2_2, s1_4-b2_2);
-    glEnd();*/
-    //No: 3x3, 3x2, 3x1, 1x4, 4x1, 4x2, 5x1, 0x2
-    //Maybe: 2x1
+    glDisable(GL_BLEND);
+
+
+    physicsFeat();
+    makeFeat();
+    usleep(200);
 
 }
