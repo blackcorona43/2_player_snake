35fa894b35fe63f17ad740966ba9eaee7200581c
stellez4 <111898074+stellez4@users.noreply.github.com>
2022-11-28 10:15:20 -0800

    Add files via upload

--- stellez.cpp
+++ stellez.cpp
@@ -138,12 +138,19 @@ void make_particles(int x, int y)
 {
     if (n >= MAX_PARTICLES)
         return;
+    
     particles[n].w = 25.0; //size of particles
     particles[n].pos[0] = x;
     particles[n].pos[1] = y;
     particles[n].vel[0] = particles[n].vel[1] = 0.0;
     ++n;
+}
 
+void make_particles2(int x, int y)
+{
+    if (n >= MAX_PARTICLES)
+        return;
+    
     particles2[n].w = 25.0; //size of particles
     particles2[n].pos[0] = x;
     particles2[n].pos[1] = y;
@@ -160,14 +167,6 @@ void physicsFeat()
         gg.movement -= 500; //how wide the group of squares are
 
     make_particles(box.pos[0]+gg.movement, box.pos[1]*2);
-
-    //particles2
-    if (gg.movement2 < -10)
-        gg.movement2 += 40; //how far apart the squares are
-    if (gg.movement2 >= -10)
-        gg.movement2 -= 500; //how wide the group of squares are
-
-    make_particles(box.pos[0]+gg.movement2, box.pos[1]*2);
 
     for (int i=0; i<n; i++)
     {
@@ -200,10 +199,31 @@ void physicsFeat()
 
 }
 
+void physicsFeat2()
+{
+    //particles2
+    if (gg.movement2 < -10)
+      gg.movement2 += 40; //how far apart the squares are
+    if (gg.movement2 >= -10)
+      gg.movement2 -= 500; //how wide the group of squares are
+
+    make_particles2(box.pos[0]+gg.movement2, box.pos[1]*2);
+
+    for (int i=0; i<n; i++)
+    {
+   	particles2[i].vel[1] -= 0.10; //speed of squares
+    	particles2[i].pos[0] += particles2[i].vel[0];
+    	particles2[i].pos[1] += particles2[i].vel[1];
+
+    if (particles2[i].pos[1] < 0.0)
+    	particles2[i] = particles2[--n];
+    }
+}
+
 void makeFeat()
 {
     //Draw all particle
-    for (int i=0; i<n; i++)
+    /*for (int i=0; i<n; i++)
     {
         glPushMatrix();
         glColor3ub(150, 160, 255);
@@ -222,10 +242,28 @@ void makeFeat()
         glVertex2f( particles2[i].w, -particles2[i].w);
         glEnd();
         glPopMatrix();
-    }
+    }*/
 
     //Draw all particle2
-    /*for (int i=0; i<n; i++)
+    for (int i=0; i<n; i++)
+      {
+      glPushMatrix();
+      glColor3ub(150, 160, 255);
+      glTranslatef(particles[i].pos[0], particles[i].pos[1], 0.0f);
+      glBegin(GL_QUADS);
+      glVertex2f(-particles[i].w, -particles[i].w);
+      glVertex2f(-particles[i].w,  particles[i].w);
+      glVertex2f( particles[i].w,  particles[i].w);
+      glVertex2f( particles[i].w, -particles[i].w);
+      glEnd();
+      glPopMatrix();
+      }
+}
+
+void makeFeat2()
+{
+    //Draw all particle2
+    for (int i=0; i<n; i++)
       {
       glPushMatrix();
       glColor3ub(150, 160, 255);
@@ -237,7 +275,7 @@ void makeFeat()
       glVertex2f( particles2[i].w, -particles2[i].w);
       glEnd();
       glPopMatrix();
-      }*/
+      }
 }
 
 unsigned int manage_state_st(unsigned int s)
@@ -294,14 +332,6 @@ void show_feature_st(int xres, int yres)
     r.center = 1;
     ggprint16(&r, 16, 0x004da6ff, "SAMANTHA'S FEATURE MODE");
 
-    //side text
-    Rect s;
-    s.left   = xres/7;
-    s.bot    = yres-700;
-    s.center = 1;
-    ggprint16(&s, 16, 0x0099d6ff, "The rain doesn't fall perfectly because wind is blowing it (;");
-
-
     //border
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //transparent
     glEnable(GL_BLEND);
@@ -346,6 +376,10 @@ void show_feature_st(int xres, int yres)
 
     physicsFeat();
     makeFeat();
+    usleep(200);
+
+    physicsFeat2();
+    makeFeat2();
     usleep(200);
 
 }
